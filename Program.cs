a4037a240e6a81e1d3aae6a222080f50fdaed893
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-03-12 00:01:17 -0300

    Tratamento de excecao no tabuleiro

--- Program.cs
+++ Program.cs
@@ -6,11 +6,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        try{
         Tabuleiro tabuleiro = new Tabuleiro(8, 8);
         tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Blakc), new Posicao(0, 0));
-        tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Blakc), new Posicao(0, 7));
+        tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Blakc), new Posicao(0, 9));
         tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Blakc), new Posicao(0, 4));
 
         Tela.ImprimirTabuleiro(tabuleiro);
+        }
+        catch(TabuleiroException e)
+        {
+            Console.WriteLine($"{e.Message}");
+        }
     }
 }
---
+++ Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,9 @@
+namespace Tabuleiro;
+
+public class TabuleiroException : Exception
+{
+    public TabuleiroException(string msg) : base (msg)
+    {
+        
+    }
+}
