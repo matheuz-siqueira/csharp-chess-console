7bde264620867511b38223e597d059febff396b5
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-03-11 23:26:36 -0300

    Criando a classe posicao

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace csharp_chess_console; 
+using Tabuleiro;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Posicao p = new Posicao(3,4); 
+        Console.WriteLine($"Posição {p}");
     }
 }
